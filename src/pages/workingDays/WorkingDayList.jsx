1305a333ae9dbaed6a5507bfedf1a3c7369b1e8c
aymenachelache <achelache.aymen@gmail.com>
2025-01-28 03:34:06 +0100

    update

--- src/pages/workingDays/WorkingDayList.jsx
+++ src/pages/workingDays/WorkingDayList.jsx
@@ -67,7 +67,7 @@ export const WorkingDaysList = ({ t }) => {
   };
 
   if (loading) {
-    return <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-opacity-75"></div>;
+    return <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-opacity-75"></div>
   }
 
   return (
